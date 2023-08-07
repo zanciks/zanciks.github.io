679ea21ef87264425da5c08970f37c742e486929
Zanciks <zackarysemancik@gmail.com>
2023-08-06 21:52:53 -0600

    song 6

--- javascript/music_player.js
+++ javascript/music_player.js
@@ -3,7 +3,8 @@ const playlist = [
     {title: 'Merry Go Round of Life - Joe Hisaishi', src: 'music/song 2.mp3'},
     {title: 'Partita for Lute in C Minor, BWV 997, Prelude - J.S Bach', src: 'music/song 3.mp3'},
     {title: 'Save the Cat (from The Bad Guys) - Daniel Pemberton', src: 'music/song 4.mp3'},
-    {title: 'Cloud Symphony - Shook', src:'music/song 5.mp3'},
+    {title: 'Cloud Symphony - Shook', src: 'music/song 5.mp3'},
+    {title: 'Nantes (instrumental) - Beirut', src: 'music/song 6.mp3'},
 ]
 
 let currentSongIndex = 0;
