fb7241b3eb553448f65c43e6ce1d2ba4c3efc404
Zanciks <zackarysemancik@gmail.com>
2024-09-28 00:24:22 -0600

    removed unneeded music

--- prototype/scripts/music.js
+++ prototype/scripts/music.js
@@ -13,18 +13,6 @@ const playlist = [
     { image: '../music/00/image.jpg', length: '2:30', title: 'The Orb of Dreamers', artist: 'Daniel Pemberton', source: '../music/00/source.mp3' },
     { image: '../music/01/image.jpg', length: '1:43', title: 'Save the Cat (from The Bad Guys)', artist: 'Daniel Pemberton', source: '../music/01/source.mp3' },
     { image: '../music/02/image.jpg', length: '3:19', title: 'I\'ll Try Anything Once', artist: 'The Strokes', source: '../music/02/source.mp3' },
-    { image: '../music/00/image.jpg', length: '0:00', title: 'SONG 004', artist: 'ARTIST 004', source: '../music/00/source.mp3' },
-    { image: '../music/00/image.jpg', length: '0:00', title: 'SONG 005', artist: 'ARTIST 005', source: '../music/00/source.mp3' },
-    { image: '../music/00/image.jpg', length: '0:00', title: 'SONG 006', artist: 'ARTIST 006', source: '../music/00/source.mp3' },
-    { image: '../music/00/image.jpg', length: '0:00', title: 'SONG 007', artist: 'ARTIST 007', source: '../music/00/source.mp3' },
-    { image: '../music/00/image.jpg', length: '0:00', title: 'SONG 008', artist: 'ARTIST 008', source: '../music/00/source.mp3' },
-    { image: '../music/00/image.jpg', length: '0:00', title: 'SONG 009', artist: 'ARTIST 009', source: '../music/00/source.mp3' },
-    { image: '../music/00/image.jpg', length: '0:00', title: 'SONG 010', artist: 'ARTIST 010', source: '../music/00/source.mp3' },
-    { image: '../music/00/image.jpg', length: '0:00', title: 'SONG 011', artist: 'ARTIST 011', source: '../music/00/source.mp3' },
-    { image: '../music/00/image.jpg', length: '0:00', title: 'SONG 012', artist: 'ARTIST 012', source: '../music/00/source.mp3' },
-    { image: '../music/00/image.jpg', length: '0:00', title: 'SONG 013', artist: 'ARTIST 013', source: '../music/00/source.mp3' },
-    { image: '../music/00/image.jpg', length: '0:00', title: 'SONG 014', artist: 'ARTIST 014', source: '../music/00/source.mp3' },
-    { image: '../music/00/image.jpg', length: '0:00', title: 'SONG 015', artist: 'ARTIST 015', source: '../music/00/source.mp3' },
 ]
 
 function togglePlay() {
@@ -84,8 +72,6 @@ function setPlaylistDisplay() {
         const rightItem = document.createElement('div');
         rightItem.className = 'item-right';
         rightItem.innerHTML = `${playlist[index].artist} (${playlist[index].length})`;
-
-
 
         songElement.appendChild(leftItem);
         songElement.appendChild(rightItem);
